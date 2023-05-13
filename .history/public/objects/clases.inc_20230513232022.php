656169df20abc4bafcf11305a96bed243a732da1
Sxcram02 <ms2d0v4@gmail.com>
2023-05-14 00:20:19 +0200

    "Añadida funcion de imagenes"

---
+++ .history/public/objects/clases.inc_20230513232022.php
@@ -0,0 +1,97 @@
+    
+    <?php
+    class SesionUser {
+        private string $sesion;
+        public function __construct($sesion){
+            $this -> sesion = $sesion;
+        }
+    }
+
+    /**
+     * Database
+     */
+    class Database {
+        private string $localhost, $serverUser, $userPass,$database;
+        private object $newObjMysql;
+        
+        /**
+         * __construct
+         *
+         * @param  mixed $localhost
+         * @param  mixed $serverUser
+         * @param  mixed $userPass
+         * @param  mixed $database
+         * @return void
+         */
+        public function __construct($localhost, $serverUser, $userPass,$database){
+            $this -> localhost = $localhost;
+            $this -> serverUser = $serverUser;
+            $this -> userPass = $userPass;
+            $this -> database = $database;
+            $newObjMysql = new mysqli("$localhost","$serverUser","$userPass","$database");
+            $this -> newObjMysql = $newObjMysql;
+        }
+        
+        /**
+         * getUser
+         *
+         * @param  mixed $client
+         * @param  mixed $pass
+         * @return bool
+         */
+        public function getUser($client,$pass):bool{
+            $mySqlConnection = $this -> newObjMysql;
+            $userQuery = "SELECT usr.clientName, usr.contrasenia FROM users usr WHERE usr.clientName='$client' AND usr.contrasenia = '$pass';";
+            $result = $mySqlConnection -> query($userQuery);
+            if($result){
+                return true;
+            }else {
+                return false;
+            }
+        }
+        
+        /**
+         * setPost
+         *
+         * @param  mixed $postDataArray
+         * @return void
+         */
+        public function setPost(array $postDataArray){
+            if(isset($postDataArray)){
+                $codBlog = rand(1,1000);
+                $columnArray = "'".implode("','",$postDataArray)."'";
+                $mysqlConnect = $this -> newObjMysql;
+                $insertBlog = "INSERT INTO blog (codBlog,titulo,imagen,subtitulo,descripcion,user) VALUES ($codBlog,$columnArray)";
+                $mysqlConnect -> query($insertBlog);
+                (mysqli_affected_rows($mysqlConnect))?true:false;
+            }else {
+                echo "Introduce los valores adecuados";
+            }
+        }
+        
+        /**
+         * getPost
+         *
+         * @param  mixed $user
+         * @return void
+         */
+        public function getPost()
+        {
+                $mySqlConnection = $this -> newObjMysql;
+                $getQuery = "SELECT blg.imagen as imagen, blg.titulo as titulo, blg.descripcion as descripcion, blg.subtitulo as subtitulo FROM blog blg JOIN users usr ON blg.user = usr.clientName WHERE usr.clientName = 'marcos';";
+
+                $result = $mySqlConnection -> query($getQuery);
+                  
+                        foreach($result as $dataPost){
+                            echo "<article>";
+                                echo "<img src='{$dataPost['imagen']}' alt='post1' />";
+                                echo $dataPost['titulo'];
+                                echo "<h1>{$dataPost['subtitulo']}</h1>";
+                                echo "<p>{$dataPost['descripcion']}</p>";
+                            echo "</article>";
+                        }
+        } 
+}
+    $sesion = new SesionUser("user");
+    $databaseConnect= new Database("localhost","root","mdv21.389863","blog");
+?>
---
+++ .history/public/src/createPost_20230513230333.php
@@ -0,0 +1,83 @@
+<?php
+$timeSession = 1000;
+session_set_cookie_params($timeSession);
+
+session_start();
+include("./../objects/clases.inc.php");
+
+if ($_SESSION['user'] != "marcos") {
+    header("Location: ./login.php");
+    exit();
+}
+
+if(isset($_POST['LOGOUT'])){
+    session_start();
+    session_unset();
+    session_destroy();
+    header("Location: ./../../index.php");
+    exit();
+}
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./../../styles/index.css">
+    <link rel="shortcut icon" href="./../../media/logo2.png" type="image/x-icon">
+    <title>New Post</title>
+</head>
+
+<body>
+    <div id="page3-root">
+        <form action="#" method="POST" enctype="multipart/form-data">
+            <header class="box-nav-bar">
+            <a href="/index.php"><h3 class="nav-title">M a r c o s</h3></a>
+                <nav class="nav-bar">
+                    <ul>
+                        <li><a href="/index.php">Home Page</a></li>
+                        <li><a href="/public/src/assets/work.php">Work</a></li>
+                        <li><a href="/public/src/assets/blog.php">Blog</a></li>
+                    </ul>
+                </nav>
+                <input type="submit" name="LOGOUT" id="LOGIN" value="logout" />
+            </header>
+            <main id="page3-container">
+                <?php
+                    if($_SERVER['REQUEST_METHOD'] == "POST"){
+                        $titulo = $_POST['titulo'];
+                        
+                        $imageName= $_FILES['image']['name'];
+                        $imagePath= $_FILES['imagen']['tmp_name'];
+                        $path = "/media";
+                        $subtitulo = $_POST['subtitulo'];
+                        $descripcion = $_POST['descripcion'];
+
+                        move_uploaded_file()
+
+                        $arrayDataPost = [$titulo,$subtitulo,$descripcion,$_SESSION['user']];
+                        if(isset($_POST['crear'])){
+                            $databaseConnect -> setPost($arrayDataPost);
+                        }
+                    }
+                ?>
+                <section>
+                    <label>Titulo del Post</label>
+                    <input type="text" name="titulo" id="page3-titulo">
+                    <label>Imagen</label>
+                    <input type="file" name="image" id="image">
+                    <label>Subtitulo</label>
+                    <input type="text" name="subtitulo" id="page3-subtitulo">
+                    <label>Descripcion</label>
+                    <input type="text" name="descripcion" id="page3-descripcion">
+                    <input type="submit" name="crear" value="crear" id="page3-crear">
+                </section>
+            </main>
+        </form>
+    </div>
+</body>
+
+</html>
